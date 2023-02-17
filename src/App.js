b2dbd5103c1b86dad4fbfdce235e554888d919ab
Greg Mansell <spockspeek@gmail.com>
2023-02-17 19:57:10 +0000

    Adds UI for game but no functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,80 @@
 import * as React from "react";
-import Button from "@mui/material/Button";
+import { Box, Divider, Typography } from "@mui/material";
+
+import man7 from "./assets/man/state7.gif";
+
+import Help from "./components/Help";
 
 function App() {
   return (
-    <div>
-      <Button variant="contained">Hello World</Button>
-    </div>
+    <Box sx={{ p: 5, textAlign: "center" }}>
+      <Typography variant="h3" component="h1">
+        HANGMAN
+      </Typography>
+
+      <Help />
+
+      <img src={man7} alt="man" />
+
+      <Box
+        sx={{
+          gap: 2,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <Typography variant="h1" component="p">
+          d
+        </Typography>
+        <Typography variant="h1" component="p">
+          _
+        </Typography>
+        <Typography variant="h1" component="p">
+          _
+        </Typography>
+        <Typography variant="h1" component="p">
+          _
+        </Typography>
+        <Typography variant="h1" component="p">
+          _
+        </Typography>
+        <Typography variant="h1" component="p">
+          _
+        </Typography>
+        <Typography variant="h1" component="p">
+          _
+        </Typography>
+        <Typography variant="h1" component="p">
+          _
+        </Typography>
+      </Box>
+
+      <Divider sx={{ my: 3 }} />
+
+      <Typography variant="h5" component="h3">
+        Guesses (3/10)
+      </Typography>
+
+      <Box
+        sx={{
+          gap: 2,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <Typography variant="h2" component="p">
+          s
+        </Typography>
+        <Typography variant="h2" component="p">
+          g
+        </Typography>
+        <Typography variant="h2" component="p">
+          x
+        </Typography>
+      </Box>
+    </Box>
   );
 }
 
---
+++ src/components/Help.js
@@ -0,0 +1,56 @@
+import * as React from "react";
+import { Box, Button, Typography, Modal, Divider } from "@mui/material";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+function App() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button onClick={handleOpen} variant="outlined">
+        HELP
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="help-title"
+        aria-describedby="help-description"
+      >
+        <Box sx={style}>
+          <Typography id="help-title" variant="h6" component="h2">
+            <strong>HANGMAN</strong>
+          </Typography>
+          <Typography id="help-description" sx={{ mt: 2 }}>
+            How to play HANGMAN
+          </Typography>
+          <Divider sx={{ my: 1 }} />
+          <Typography variant="body2">
+            1. When the game loads, we pick a word from our dictionary.
+            <br />
+            2. It's your task to guess the word.
+            <br />
+            3. You get <strong>10 guesses</strong>.
+            <br />
+            4. Guess the word, and you win, but guess wrong 10 times, and you
+            lose!
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
+
+export default App;
