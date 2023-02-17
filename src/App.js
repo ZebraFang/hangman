bb33d3fec2cbaf25f7dbcee3fec9f15303b8e6d8
Greg Mansell <spockspeek@gmail.com>
2023-02-17 20:57:27 +0000

    Adds random word selection from dictionary.txt

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Divider, Typography } from "@mui/material";
 
 import man7 from "./assets/man/state7.gif";
@@ -6,6 +6,43 @@ import man7 from "./assets/man/state7.gif";
 import Help from "./components/Help";
 
 function App() {
+  const [word, setWord] = useState("");
+
+  // Use a side effect to load the dictionary and select a random word
+  useEffect(() => {
+    // Load the dictionary file
+    //  parse it, and then,
+    //  select a random word from the list
+    const getDictionary = async () => {
+      const loaded = await fetch("/dictionary.txt");
+
+      if (loaded) {
+        const text = await loaded.text();
+
+        // get rid of the header text
+        const withoutHeader = text.split("START\n")[1];
+
+        // split the rest of the dictionary into an array we can use
+        const words = withoutHeader.split("\n");
+
+        // pick a random word
+        const wordNumber = Math.round(Math.random() * words.length);
+        const selectedWord = words[wordNumber].toLowerCase();
+
+        // set the state, and begin the game!
+        console.log({ selectedWord });
+        setWord(selectedWord);
+
+        //
+      } else {
+        // Could not load the dictionary!
+        console.error("Could not load the dictionary!");
+      }
+    };
+
+    getDictionary();
+  }, []);
+
   return (
     <Box sx={{ p: 5, textAlign: "center" }}>
       <Typography variant="h3" component="h1">
@@ -24,30 +61,13 @@ function App() {
           justifyContent: "center",
         }}
       >
-        <Typography variant="h1" component="p">
-          d
-        </Typography>
-        <Typography variant="h1" component="p">
-          _
-        </Typography>
-        <Typography variant="h1" component="p">
-          _
-        </Typography>
-        <Typography variant="h1" component="p">
-          _
-        </Typography>
-        <Typography variant="h1" component="p">
-          _
-        </Typography>
-        <Typography variant="h1" component="p">
-          _
-        </Typography>
-        <Typography variant="h1" component="p">
-          _
-        </Typography>
-        <Typography variant="h1" component="p">
-          _
-        </Typography>
+        {word.split("").map((letter, i) => {
+          return (
+            <Typography key={`letter-${i}`} variant="h1" component="p">
+              {letter}
+            </Typography>
+          );
+        })}
       </Box>
 
       <Divider sx={{ my: 3 }} />
